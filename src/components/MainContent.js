32a74a3a2d47d2f82a4d44814e38ac99c459ad29
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-02-14 20:37:44 +0300

    completed assignment

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,14 @@
 import React from "react";
+import GenrePicker from "./GenrePicker";
+import AlbumList from "./AlbumList";
+
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return <main>
+    <GenrePicker />
+    <AlbumList />
+
+    {/* What components go here? */}</main>;
 }
 
 export default MainContent;
